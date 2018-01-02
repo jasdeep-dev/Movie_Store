4435419000223252f59bf57b1c13eb5d565a21e0
jasdeepkaur15 <jasdeepg21@gmail.com>
2018-01-02 12:47:49 +0530

    SearchButton

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -58,11 +58,16 @@ before_action :authenticate_admin_user!,only: [:create]
 		@view = params[:view]
 		gon.view = @view
 		search = params[:search]
+		 capital_search = search.capitalize
+       	 downcase_search = search.downcase
+       	 upcase_search = search.upcase
+       	 title_search = search.titleize
 
 		if search
 		
        
-      		@home_page_movie = Movie.where("title like?","#{search}%").order('rating ASC')
+      		@home_page_movie= Movie.where("title like? OR title like? OR title like? OR title like?","#{capital_search}%","#{downcase_search}%","#{upcase_search}%","#{title_search}%").order('rating ASC')
+
   		else
   			@home_page_movie = Movie.order('rating DESC')
       	end
